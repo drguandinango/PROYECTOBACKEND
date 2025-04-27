a1fb83b6a823cef625dce0057f1db55bf958ab3b
drguandinango <drguandinango@pucesi.edu.ec>
2025-04-27 14:57:54 -0500

    se HA REALIZADO la  conección  con FASTPI

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ async def create_user(
     user: _schemas.UserCreate, db: _orm.Session = _fastapi.Depends(_services.get_db)
 ):
     
-#"""minuto 4:12 """
+#""" se conencta con FASTPIminuto 4:12 """
     db_user = await _services.get_user_by_email(user.email, db)
     if db_user:
         raise _fastapi.HTTPException(status_code=400, detail="Email already in use")
